5ae58543210e5ccda925882c443cfef629e32ceb
didepanlayar <albinmaulana@yahoo.com>
2026-05-10 18:20:40 +0700

    Feature: Filament - Manage Wedding Organizers

--- app/Filament/Resources/WeddingOrganizerResource.php
+++ app/Filament/Resources/WeddingOrganizerResource.php
@@ -6,9 +6,13 @@ use App\Filament\Resources\WeddingOrganizerResource\Pages;
 use App\Filament\Resources\WeddingOrganizerResource\RelationManagers;
 use App\Models\WeddingOrganizer;
 use Filament\Forms;
+use Filament\Forms\Components\FileUpload;
+use Filament\Forms\Components\TextInput;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Tables;
+use Filament\Tables\Columns\ImageColumn;
+use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
@@ -17,13 +21,24 @@ class WeddingOrganizerResource extends Resource
 {
     protected static ?string $model = WeddingOrganizer::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationIcon = 'heroicon-o-book-open';
 
     public static function form(Form $form): Form
     {
         return $form
             ->schema([
-                //
+                TextInput::make('name')
+                    ->required()
+                    ->maxLength(255),
+                TextInput::make('phone')
+                    ->required()
+                    ->maxLength(255),
+                FileUpload::make('icon')
+                    ->required()
+                    ->image()
+                    ->disk('public')
+                    ->directory('organizer')
+                    ->visibility('public'),
             ]);
     }
 
@@ -31,7 +46,11 @@ class WeddingOrganizerResource extends Resource
     {
         return $table
             ->columns([
-                //
+                TextColumn::make('name')
+                    ->searchable(),
+                TextColumn::make('phone'),
+                ImageColumn::make('icon')
+                    ->disk('public'),
             ])
             ->filters([
                 //
