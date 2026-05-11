6be19e2e3e3cab86842763e071d8bd9718ac207c
didepanlayar <albinmaulana@yahoo.com>
2026-05-11 11:43:39 +0700

    Feature: Filament - Manage Wedding Testimonials

--- app/Filament/Resources/WeddingTestimonialResource.php
+++ app/Filament/Resources/WeddingTestimonialResource.php
@@ -6,9 +6,15 @@ use App\Filament\Resources\WeddingTestimonialResource\Pages;
 use App\Filament\Resources\WeddingTestimonialResource\RelationManagers;
 use App\Models\WeddingTestimonial;
 use Filament\Forms;
+use Filament\Forms\Components\FileUpload;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\Textarea;
+use Filament\Forms\Components\TextInput;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Tables;
+use Filament\Tables\Columns\ImageColumn;
+use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
@@ -17,13 +23,32 @@ class WeddingTestimonialResource extends Resource
 {
     protected static ?string $model = WeddingTestimonial::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationIcon = 'heroicon-o-chat-bubble-left-ellipsis';
 
     public static function form(Form $form): Form
     {
         return $form
             ->schema([
-                //
+                TextInput::make('name')
+                    ->required()
+                    ->maxLength(255),
+                TextInput::make('occupation')
+                    ->required()
+                    ->maxLength(255),
+                FileUpload::make('photo')
+                    ->required()
+                    ->image()
+                    ->disk('public')
+                    ->directory('testimonials')
+                    ->visibility('public'),
+                Select::make('wedding_package_id')
+                    ->relationship('weddingPackage', 'name')
+                    ->label('Wedding Package')
+                    ->required()
+                    ->preload()
+                    ->searchable(),
+                Textarea::make('message')
+                    ->required(),
             ]);
     }
 
@@ -31,7 +56,12 @@ class WeddingTestimonialResource extends Resource
     {
         return $table
             ->columns([
-                //
+                ImageColumn::make('photo')
+                    ->circular(),
+                TextColumn::make('name')
+                    ->searchable(),
+                TextColumn::make('weddingPackage.name')
+                    ->label('Wedding Package'),
             ])
             ->filters([
                 //
