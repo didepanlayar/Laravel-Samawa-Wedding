ca364c35022b453f5c94817a0704ef7ab8ecee64
didepanlayar <albinmaulana@yahoo.com>
2026-05-14 18:11:08 +0700

    Feature: Query API - WeddingTestimonialsController

--- app/Http/Controllers/Api/WeddingTestimonialController.php
+++ app/Http/Controllers/Api/WeddingTestimonialController.php
@@ -3,9 +3,18 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\Api\WeddingTestimonialResource;
+use App\Models\WeddingTestimonial;
 use Illuminate\Http\Request;
 
 class WeddingTestimonialController extends Controller
 {
-    //
+    public function index(Request $request)
+    {
+        $limit = $request->query('limit', 10);
+
+        $weddingTestimonials = WeddingTestimonial::with(['weddingPackage'])->limit($limit)->get();
+
+        return WeddingTestimonialResource::collection($weddingTestimonials);
+    }
 }
